6017d640f0edb18f35af340e41eb6a8048960de3
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-05-20 23:23:09 +0800

    added a new learnig of interaction owith the user

--- python-day3.py
+++ python-day3.py
@@ -1,7 +1,19 @@
-print("tell me anything...")
-anything = input()
-print("Hmm...", anything, "...Really?")
+#print("tell me anything...")
+#anything = input()
+#print("Hmm...", anything, "...Really?")
 
+#anything = input("Tell me anything... ")
+#print("Hmm..", anything, "...Really?")
 
+#anything = input("Enter a number: ")
+#something = anything ** 2.0
+#print(anything, "to the power of 2 is", something)
 
+#anything = float(input("Ennter a number:"))
+#simething = anything ** 2.0
+#print(anything, "to the power of 2 is", simething)
 
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("input second leg length: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("Hypotenuse length is", hypo)
