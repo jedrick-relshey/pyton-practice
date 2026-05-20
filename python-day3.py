e7f42ce4dd3571e23bb2bff1e358c74f8a0c17e3
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-05-21 00:01:44 +0800

    added a day3 learning of python

--- python-day3.py
+++ python-day3.py
@@ -1,6 +1,7 @@
 #print("tell me anything...")
 #anything = input()
 #print("Hmm...", anything, "...Really?")
+from idlelib.colorizer import prog_group_name_to_tag
 
 #anything = input("Tell me anything... ")
 #print("Hmm..", anything, "...Really?")
@@ -13,7 +14,19 @@
 #simething = anything ** 2.0
 #print(anything, "to the power of 2 is", simething)
 
-leg_a = float(input("Input first leg length: "))
-leg_b = float(input("input second leg length: "))
-hypo = (leg_a**2 + leg_b**2) ** .5
-print("Hypotenuse length is", hypo)
+#leg_a = float(input("Input first leg length: "))
+#leg_b = float(input("input second leg length: "))
+#print("Hypotenuse length is", (leg_a**2 + leg_b**2) ** .5)
+
+#fnam = input("May I have your first name,please? ")
+#lnam = input("May I have your last name,please? ")
+#print("thank you")
+#print("\nYour name is " + fnam + " " + lnam + ".")
+
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+
+print(" " * 1 + 20 * "-")
+print(("|" + " " * 20 + "|\n") *5, end="")
+print(" " * 1 + 20 * "-")
