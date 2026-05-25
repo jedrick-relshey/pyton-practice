65caf699b8a33a88b170eb0c0f5739785771d807
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-05-26 01:23:45 +0800

    added a day 5 file

---
+++ python-day-5.py
@@ -0,0 +1,47 @@
+#LOOP IN PYTHON
+#largest_number = -999999999
+#number = int(input("Enter a number: "))
+#while number != -1:
+    #if number > largest_number:
+        #largest_number = number
+    #number = int(input("Enter a number: "))
+#print("The largest number is: ", largest_number)
+
+#odd_number = 0
+#even_number = 0
+#number = int(input("Enter a number or type 0 to stop: "))
+#while number != 0:
+    #if number % 2 == 0:
+        #odd_number += 1
+    #else:
+        #even_number += 1
+    #number = int(input("Enter a number or type 0 to stop: "))
+#print("Odd numbers count: ", odd_number)
+#print("Even numbers count: ", even_number)
+
+#counter = 5
+#while counter != 0:
+    #print("Inside The loop: ", counter)
+    #counter -= 1
+#print("Outside The loop: ", counter)
+
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+number = int(input("Enter an number: "))
+while number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    number = int(input("Enter the secret number: "))
+
+print(secret_number)
+print("Well done!")
--- python-day3.py
+++ python-day3.py
@@ -23,10 +23,38 @@ from idlelib.colorizer import prog_group_name_to_tag
 #print("thank you")
 #print("\nYour name is " + fnam + " " + lnam + ".")
 
-print("+" + 10 * "-" + "+")
-print(("|" + " " * 10 + "|\n") * 5, end="")
-print("+" + 10 * "-" + "+")
+#print("+" + 10 * "-" + "+")
+#print(("|" + " " * 10 + "|\n") * 5, end="")
+#print("+" + 10 * "-" + "+")
 
-print(" " * 1 + 20 * "-")
-print(("|" + " " * 20 + "|\n") *5, end="")
-print(" " * 1 + 20 * "-")
+#print(" " * 1 + 20 * "-")
+#print(("|" + " " * 20 + "|\n") *5, end="")
+#print(" " * 1 + 20 * "-")
+
+#leg_a = float(input("Input first leg length: "))
+#leg_b = float(input("Input second leg length: "))
+#print("Hypotenuse length is " + str((leg_a**2 + leg_b**2) ** .5))
+
+#a = float(input("Enter first number: "))
+#b = float(input("Enter second number: "))
+#print("addition here: ", a+b)
+#print("subtraction here: ", a-b)
+#print("multiplication here: ", a*b)
+#print("division here: ", a/b)
+#print("\nThat's all, folks!")
+
+#x = float(input("Enter value for x: "))
+#y = 1./(x + 1./(x + 1./(x + 1./x)))
+#print("y = ", y)
+
+#hour = int(input("Starting tine (hours): "))
+#mins = int(input("Starting time (minutes): "))
+#dura = int(input("Event duration (minutes): "))
+#mins = mins + dura
+#hour = hour + mins // 60
+#mins = mins % 60
+#hour = hour % 24
+#print(hour, ":", mins, sep='')
+
+y = 2 + 3 * 5.
+print(y)
