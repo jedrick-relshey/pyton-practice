c0063144b7547a2999d2196934440ea180c5d5aa
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-06-24 23:30:31 +0800

    added a code

--- python-day-5.py
+++ python-day-5.py
@@ -42,18 +42,5 @@ from operator import truediv
 #print(secret_number)
 #print("Well done!")
 
-x = 1
-z = 2.9
-y = 1j
-
-a = float(x)
-b = int(z)
-c = complex(x)
-
-print(a)
-print(b)
-print(c)
-
-print(type(a))
-print(type(b))
-print(type(c))
+b = "jedjed. miclat"
+print(b[-5:])
