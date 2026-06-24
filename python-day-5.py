34f3a7907df06fdabde34fb6a84863267b620500
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-06-24 21:57:53 +0800

    added a code

--- python-day-5.py
+++ python-day-5.py
@@ -6,6 +6,7 @@
         #largest_number = number
     #number = int(input("Enter a number: "))
 #print("The largest number is: ", largest_number)
+from operator import truediv
 
 #odd_number = 0
 #even_number = 0
@@ -25,23 +26,27 @@
     #counter -= 1
 #print("Outside The loop: ", counter)
 
-secret_number = 777
+#secret_number = 777
+#print(
+#"""+================================+
+#| Welcome to my game, muggle!    |
+#| Enter an integer number        |
+#| and guess what number I've     |
+#| picked for you.                |
+#| So, what is the secret number? |
+#+================================+""")
+#number = int(input("Enter an number: "))
+#while number != secret_number:
+    #print("Ha ha! You're stuck in my loop!")
+    #number = int(input("Enter the secret number: "))
+#print(secret_number)
+#print("Well done!")
 
-print(
-"""
-+================================+
-| Welcome to my game, muggle!    |
-| Enter an integer number        |
-| and guess what number I've     |
-| picked for you.                |
-| So, what is the secret number? |
-+================================+
-""")
+x = "jedrruck"
 
-number = int(input("Enter an number: "))
-while number != secret_number:
-    print("Ha ha! You're stuck in my loop!")
-    number = int(input("Enter the secret number: "))
+def myfunc():
+    global x
+    x = "miclat"
 
-print(secret_number)
-print("Well done!")
+myfunc()
+print("Hi " + x)
